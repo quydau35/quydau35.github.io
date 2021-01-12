42beeb152cc7d8d6afd80469bb40b2a18276999a
ngan.nhq <ngan.nhq@liena.vn>
2021-01-12 18:39:26 +0700

    finish chunk 2 details

--- ds/chunk_2/chunk_2.py
+++ ds/chunk_2/chunk_2.py
@@ -184,4 +184,85 @@ class c4_string():
 
 class c5_operators():
     """
+    Operators are used to perform operations on variables and values.\n
+    In the example below, we use the ```+``` operator to add together two values:\n
+    ```
+    print(10 + 5)
+    ```
+    Python divides the operators in the following groups:\n
+        Arithmetic operators\n
+        Assignment operators\n
+        Comparison operators\n
+        Logical operators\n
+        Identity operators\n
+        Membership operators\n
+        Bitwise operators\n
+    Arithmetic operators are used with numeric values to perform common mathematical operations:\n
+    ```
+    Operator	Name	Example
+    +	Addition	x + y
+    -	Subtraction	x - y
+    *	Multiplication	x * y
+    /	Division	x / y
+    %	Modulus	x % y
+    **	Exponentiation	x ** y
+    //	Floor division	x // y
+    ```
+    Assignment operators are used to assign values to variables:\n
+    ```
+    Operator	Example	Same As
+    =	x = 5	x = 5
+    +=	x += 3	x = x + 3
+    -=	x -= 3	x = x - 3
+    *=	x *= 3	x = x * 3
+    /=	x /= 3	x = x / 3
+    %=	x %= 3	x = x % 3
+    //=	x //= 3	x = x // 3
+    **=	x **= 3	x = x ** 3
+    &=	x &= 3	x = x & 3
+    |=	x |= 3	x = x | 3
+    ^=	x ^= 3	x = x ^ 3
+    >>=	x >>= 3	x = x >> 3
+    <<=	x <<= 3	x = x << 3
+    ```
+    Comparison operators are used to compare two values:\n
+    ```
+    Operator	Name	Example
+    ==	Equal	x == y
+    !=	Not equal	x != y
+    >	Greater than	x > y
+    <	Less than	x < y
+    >=	Greater than or equal to	x >= y
+    <=	Less than or equal to	x <= y
+    ```
+    Logical operators are used to combine conditional statements:\n
+    ```
+    Operator	Description	Example
+    and 	Returns True if both statements are true	x < 5 and  x < 10
+    or	Returns True if one of the statements is true	x < 5 or x < 4
+    not	Reverse the result, returns False if the result is true	not(x < 5 and x < 10)
+    ```
+    Identity operators are used to compare the objects, not if they are equal, but if they are actually the same object, with the same memory location:\n
+    ```
+    Operator	Description	Example
+    is 	Returns True if both variables are the same object	x is y
+    is not	Returns True if both variables are not the same object	x is not y
+    ```
+    Membership operators are used to test if a sequence is presented in an object:\n
+    ```
+    Operator	Description	Example
+    in 	Returns True if a sequence with the specified value is present in the object	x in y
+    not in	Returns True if a sequence with the specified value is not present in the object	x not in y
+    ```
+    Bitwise operators are used to compare (binary) numbers:
+
+    ```
+    Operator	Name	Description
+    & 	AND	Sets each bit to 1 if both bits are 1
+    |	OR	Sets each bit to 1 if one of two bits is 1
+    ^	XOR	Sets each bit to 1 if only one of two bits is 1
+    ~ 	NOT	Inverts all the bits
+    <<	Zero fill left shift	Shift left by pushing zeros in from the right and let the leftmost bits fall off
+    >>	Signed right shift	Shift right by pushing copies of the leftmost bit in from the left, and let the rightmost bits fall off
+    ```
     """
